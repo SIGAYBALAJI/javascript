82cae36997b23a679d2f0bd055c7555f00aff462
SIGAYBALAJI <142469660+SIGAYBALAJI@users.noreply.github.com>
2024-10-02 20:54:39 +0530

    Add files via upload

---
+++ Myscript.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    document.getElementById("demo").innerHTML="content changed"
+    document.getElementById("demo").style.display='none'
+    document.getElementById("demo1").style.color='green'
+   
+}
